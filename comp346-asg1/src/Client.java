949c664b6a4bf7c7db4ef0f1745a02074c47ef11
Stella Nguyen <stellakimnguyen@gmail.com>
2020-03-08 17:03:52 -0400

    Set up repo

---
+++ comp346-asg1/src/Client.java
@@ -0,0 +1,227 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Client class
+ *
+ * @author Kerly Titus
+ */
+
+public class Client extends Thread {
+
+    private static int numberOfTransactions;        /* Number of transactions to process */
+    private static int maxNbTransactions;            /* Maximum number of transactions */
+    private static Transactions[] transaction;    /* Transactions to be processed */
+    private static Network objNetwork;            /* Client object to handle network operations */
+    private String clientOperation;                    /* sending or receiving */
+
+    /**
+     * Constructor method of Client class
+     *
+     * @param
+     * @return
+     */
+    Client(String operation) {
+        if (operation.equals("sending")) {
+            System.out.println("\n Initializing client sending application ...");
+            numberOfTransactions = 0;
+            maxNbTransactions = 100;
+            transaction = new Transactions[maxNbTransactions];
+            objNetwork = new Network("client");
+            clientOperation = operation;
+            System.out.println("\n Initializing the transactions ... ");
+            readTransactions();
+            System.out.println("\n Connecting client to network ...");
+            String cip = objNetwork.getClientIP();
+            if (!(objNetwork.connect(cip))) {
+                System.out.println("\n Terminating client application, network unavailable");
+                System.exit(0);
+            }
+        } else if (operation.equals("receiving")) {
+            System.out.println("\n Initializing client receiving application ...");
+            clientOperation = operation;
+        }
+    }
+
+    /**
+     * Accessor method of Client class
+     *
+     * @param
+     * @return numberOfTransactions
+     */
+    public int getNumberOfTransactions() {
+        return numberOfTransactions;
+    }
+
+    /**
+     * Mutator method of Client class
+     *
+     * @param nbOfTrans
+     * @return
+     */
+    public void setNumberOfTransactions(int nbOfTrans) {
+        numberOfTransactions = nbOfTrans;
+    }
+
+    /**
+     * Accessor method of Client class
+     *
+     * @param
+     * @return clientOperation
+     */
+    public String getClientOperation() {
+        return clientOperation;
+    }
+
+    /**
+     * Mutator method of Client class
+     *
+     * @param operation
+     * @return
+     */
+    public void setClientOperation(String operation) {
+        clientOperation = operation;
+    }
+
+    /**
+     * Reading of the transactions from an input file
+     *
+     * @param
+     * @return
+     */
+    public void readTransactions() {
+        Scanner inputStream = null;     /* Transactions input file stream */
+        int i = 0;                      /* Index of transactions array */
+
+        try {
+            inputStream = new Scanner(new FileInputStream("C:\\Users\\Stella Nguyen .DESKTOP-EP77V4A\\Documents\\UNI\\WINTER 2020\\COMP 346\\Assignments\\comp346-asg1_FINAL\\comp346-asg1\\transaction.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println("File transaction.txt was not found");
+            System.out.println("or could not be opened.");
+            System.exit(0);
+        }
+        while (inputStream.hasNextLine()) {
+            try {
+                transaction[i] = new Transactions();
+                transaction[i].setAccountNumber(inputStream.next());            /* Read account number */
+                transaction[i].setOperationType(inputStream.next());            /* Read transaction type */
+                transaction[i].setTransactionAmount(inputStream.nextDouble());  /* Read transaction amount */
+                transaction[i].setTransactionStatus("pending");                 /* Set current transaction status */
+                i++;
+            } catch (InputMismatchException e) {
+                System.out.println("Line " + i + "file transactions.txt invalid input");
+                System.exit(0);
+            }
+
+        }
+        setNumberOfTransactions(i);        /* Record the number of transactions processed */
+
+       // System.out.println("\n DEBUG : Client.readTransactions() - " + getNumberOfTransactions() + " transactions processed");
+
+        inputStream.close();
+
+    }
+
+    /**
+     * Sending the transactions to the server
+     *
+     * @param
+     * @return
+     */
+    public void sendTransactions() {
+        int i = 0;     /* index of transaction array */
+
+        while (i < getNumberOfTransactions()) {
+             while( objNetwork.getInBufferStatus().equals("full")) {
+                 // /* Alternatively, busy-wait until the network input buffer is available */
+//                 System.out.println("FULL");
+                 Thread.yield();
+//                 System.out.println("\n Inbuffer FULL");
+             };
+
+             transaction[i].setTransactionStatus("sent");   /* Set current transaction status */
+
+           // System.out.println("\n DEBUG : Client.sendTransactions() - sending transaction on account " + transaction[i].getAccountNumber());
+
+            objNetwork.send(transaction[i]);                            /* Transmit current transaction */
+            i++;
+        }
+
+    }
+
+    /**
+     * Receiving the completed transactions from the server
+     *
+     * @param transact
+     * @return
+     */
+    public void receiveTransactions(Transactions transact) {
+        int i = 0;     /* Index of transaction array */
+
+        while (i < getNumberOfTransactions()) {
+             while( objNetwork.getOutBufferStatus().equals("empty")){
+                 /* Alternatively, busy-wait until the network output buffer is available */
+//                 System.out.println(" EMPTY");
+                 Thread.yield();             // yield the thread until it won't be empty anymore
+//                 System.out.println("\n Outbuffer EMPTY");
+             };
+//            while( objNetwork.getInBufferStatus().equals("full")) {
+//                // /* Alternatively, busy-wait until the network input buffer is available */
+//                Thread.yield();
+////                 System.out.println("\n Inbuffer FULL");
+//            };
+            objNetwork.receive(transact);                                /* Receive updated transaction from the network buffer */
+
+           // System.out.println("\n DEBUG : Client.receiveTransactions() - receiving updated transaction on account " + transact.getAccountNumber());
+
+            System.out.println(transact);                                /* Display updated transaction */
+            i++;
+        }
+    }
+
+    /**
+     * Create a String representation based on the Client Object
+     *
+     * @param
+     * @return String representation
+     */
+    public String toString() {
+        return ("\n client IP " + objNetwork.getClientIP() + " Connection status" + objNetwork.getClientConnectionStatus() + "Number of transactions " + getNumberOfTransactions());
+    }
+
+    /**
+     * Code for the run method
+     *
+     * @param
+     * @return
+     */
+    public void run() {
+        Transactions transact = new Transactions();
+        long sendClientStartTime, sendClientEndTime, receiveClientStartTime, receiveClientEndTime;
+        objNetwork.connect(objNetwork.getClientIP());
+        /* Implement the code for the run method */
+        if(this.getClientOperation().equals("sending")) {
+
+            sendClientStartTime = System.currentTimeMillis();
+            sendTransactions();
+            sendClientEndTime = System.currentTimeMillis();
+        }
+        // else it will be receiving
+        else {
+
+            receiveClientStartTime = System.currentTimeMillis();
+            receiveTransactions(transact);
+            receiveClientEndTime = System.currentTimeMillis();
+//            objNetwork.disconnect(objNetwork.getClientIP());
+            objNetwork.setClientConnectionStatus("disconnected");
+        }
+    }
+}
